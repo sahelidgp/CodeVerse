acad8b243fb978a3cfe7544ef4860e80ec7c454d
sahelidgp <sahelimahanty25@gmail.com>
2026-06-18 22:08:48 +0530

    database and tool setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,18 @@
 import express from "express";
 import path from "path"
+import dns from "dns";
 import { ENV } from './lib/env.js'
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const app = express();
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 const __dirname = path.resolve()
-
 app.get('/books',(req,res)=>{
     res.status(200).json({msg:"this is the book endpoint"})
 })
@@ -18,4 +25,17 @@ if(ENV.NODE_ENV === "production"){
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
     })
 }
-app.listen(ENV.PORT,()=> console.log(`Server is running on port ${ENV.PORT}`))
+
+
+const startServer = async() => {
+    try{
+        await connectDB();
+        app.listen(ENV.PORT,()=>console.log(`Server is running on port ${ENV.PORT}`));
+     
+    
+    }catch(error){
+        console.error("💥Error starting the server",error);
+    }   
+
+};
+startServer();
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './App.css'
 import App from './App.jsx'
-
+import { ClerkProvider } from "@clerk/clerk-react";
+const VITE_CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+  if(!VITE_CLERK_PUBLISHABLE_KEY){
+    throw new Error('Missing Publishable key');
+    
+  }
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={VITE_CLERK_PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 )
