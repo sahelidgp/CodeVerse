2fc2765d82833c51a7c010f4315ad2610b0157a5
sahelidgp <sahelimahanty25@gmail.com>
2026-06-20 14:08:12 +0530

    webhook applied

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,22 @@
 import express from "express";
 import path from "path"
+import { Webhook } from 'svix';
+import User from "./models/User.js";
 import dns from "dns";
+import bodyParser from 'body-parser';
+
 import { ENV } from './lib/env.js'
 import { connectDB } from "./lib/db.js";
-import cors from "cors";
-import { serve } from "inngest/express"
-import { inngest, functions } from "./lib/inngest.js";
+//import cors from "cors";
+// import { serve } from "inngest/express"
+// import { inngest, functions } from "./lib/inngest.js";
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const app = express();
-app.use(express.json());
 //credentials:true means server allows a browser to include cookies on request
-app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+//app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
 
-app.use("/api/inngest",serve({client: inngest, functions}));
+//app.use("/api/inngest",serve({client: inngest, functions}));
 
 const __dirname = path.resolve()
 app.get('/books',(req,res)=>{
@@ -42,3 +45,48 @@ const startServer = async() => {
 
 };
 startServer();
+app.post(
+  '/api/webhooks',
+  bodyParser.raw({ type: 'application/json' }),
+  async function (req, res) {
+    try {
+      const payloadString = req.body.toString();
+      const svixHeaders = req.headers;
+
+      const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET_KEY);
+      const evt = wh.verify(payloadString, svixHeaders);
+
+      const { id, ...attributes } = evt.data;
+
+      const eventType = evt.type;
+
+      if (eventType === 'user.created') {
+        const firstName = attributes.first_name;
+        const lastName = attributes.last_name;
+
+        console.log(firstName);
+
+        const user = new User({
+          clerkUserId: id,
+          firstName: firstName,
+          lastName: lastName,
+        });
+
+        await user.save();
+        console.log('User is created');
+        // console.log(`User ${id} is ${eventType}`);
+        // console.log(attributes);
+      }
+
+      res.status(200).json({
+        success: true,
+        message: 'Webhook received',
+      });
+    } catch (err) {
+      res.status(400).json({
+        success: false,
+        message: err.message,
+      });
+    }
+  }
+);
