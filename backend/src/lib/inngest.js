5a2bb2b7ac9f17636932b2b9960e458859cc7628
sahelidgp <sahelimahanty25@gmail.com>
2026-06-22 02:48:47 +0530

    modify inngest file

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -8,28 +8,37 @@ export const inngest = new Inngest({ id: "code-verse" });
 const syncUser = inngest.createFunction(
   { id: "sync-user" },
   { event: "clerk/user.created" },
-  async ({ event }) => {
-    await connectDB();
+  async ({ event, step }) => { // 👈 Added 'step' here
 
     const { id, email_addresses, first_name, last_name, image_url } = event.data;
+
+    // Stream hates empty names. This ensures they always get a valid string.
+    const sanitizedName = `${first_name || ""} ${last_name || ""}`.trim() || "New User";
 
     const newUser = {
       clerkId: id,
       email: email_addresses[0]?.email_address,
-      name: `${first_name || ""} ${last_name || ""}`,
-      profileImage: image_url,
+      name: sanitizedName,
+      profileImage: image_url || "",
     };
 
-   // await User.create(newUser);
-await User.findOneAndUpdate(
-      { clerkId: id }, // Look for this user
-      newUser,         // The data to save
-      { upsert: true, new: true } // If they don't exist, create them. If they do, update them!
-    );
-    await upsertStreamUser({
-      id: newUser.clerkId.toString(),
-      name: newUser.name,
-      image: newUser.profileImage,
+    // STEP 1: MongoDB Sync
+    await step.run("Save to MongoDB", async () => {
+      await connectDB();
+      await User.findOneAndUpdate(
+        { clerkId: id }, 
+        newUser,         
+        { upsert: true, new: true } 
+      );
+    });
+
+    // STEP 2: Stream Sync
+    await step.run("Save to Stream", async () => {
+      await upsertStreamUser({
+        id: newUser.clerkId.toString(),
+        name: newUser.name,
+        image: newUser.profileImage,
+      });
     });
   }
 );
@@ -37,13 +46,17 @@ await User.findOneAndUpdate(
 const deleteUserFromDB = inngest.createFunction(
   { id: "delete-user-from-db" },
   { event: "clerk/user.deleted" },
-  async ({ event }) => {
-    await connectDB();
-
+  async ({ event, step }) => { // 👈 Added 'step' here
     const { id } = event.data;
-    await User.deleteOne({ clerkId: id });
 
-    await deleteStreamUser(id.toString());
+    await step.run("Delete from MongoDB", async () => {
+      await connectDB();
+      await User.deleteOne({ clerkId: id });
+    });
+
+    await step.run("Delete from Stream", async () => {
+      await deleteStreamUser(id.toString());
+    });
   }
 );
 
