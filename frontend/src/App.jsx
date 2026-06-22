383b4953a083f6b934b6cbfcef686cfdcd609bae
sahelidgp <sahelimahanty25@gmail.com>
2026-06-22 23:29:39 +0530

    problems page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import HomePage from "./pages/HomePage";
 import { Toaster } from "react-hot-toast";
 //import DashboardPage from "./pages/DashboardPage";
 // import ProblemPage from "./pages/ProblemPage";
-// import ProblemsPage from "./pages/ProblemsPage";
+import ProblemsPage from "./pages/ProblemsPage";
 // import SessionPage from "./pages/SessionPage";
  
 function App() {
@@ -22,7 +22,8 @@ function App() {
         <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} /> 
         <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} /> */}
 
-        {/* <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+         <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+         {/*
         <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
         <Route path="/session/:id" element={isSignedIn ? <SessionPage /> : <Navigate to={"/"} />} /> */} 
       </Routes>
