8e9fba220656dfd7b58d71762e2e6ec601d3013b
sahelidgp <sahelimahanty25@gmail.com>
2026-06-22 21:52:59 +0530

    frontend tools setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,35 @@
-   import './App.css'
-import { SignedIn, SignedOut, SignInButton,SignOutButton, UserButton } from '@clerk/clerk-react';
+import { useUser } from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
 
-function App() {
- return(
-  <>
-  <h1>WELCOME TO THE APP</h1>
-  <SignedOut>
-    <SignInButton mode="modal"/>
-  </SignedOut>
+import { Toaster } from "react-hot-toast";
+// import DashboardPage from "./pages/DashboardPage";
+// import ProblemPage from "./pages/ProblemPage";
+// import ProblemsPage from "./pages/ProblemsPage";
+// import SessionPage from "./pages/SessionPage";
  
+function App() {
+  const { isSignedIn, isLoaded } = useUser();
 
-  <SignedIn>
-    <SignOutButton/>
-  </SignedIn>
+  // this will get rid of the flickering effect
+  if (!isLoaded) return null;
 
-  <UserButton/>
-   </>
- )
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={ <HomePage />} />
+
+        {/* <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
+        <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
+
+        <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+        <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
+        <Route path="/session/:id" element={isSignedIn ? <SessionPage /> : <Navigate to={"/"} />} /> */}
+      </Routes>
+
+      <Toaster toastOptions={{ duration: 3000 }} />
+    </>
+  );
 }
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './App.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
-const VITE_CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-  if(!VITE_CLERK_PUBLISHABLE_KEY){
-    throw new Error('Missing Publishable key');
-    
-  }
-createRoot(document.getElementById('root')).render(
+import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Clerk Publishable Key");
+}
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider publishableKey={VITE_CLERK_PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+          <App />
+        </ClerkProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
