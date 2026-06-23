7eea6213ddc2a8d1273af9ed4af7d7eae5b06a78
sahelidgp <sahelimahanty25@gmail.com>
2026-06-23 20:50:48 +0530

    fixing the deployment issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,9 +36,9 @@ app.use("/api/sessions", sessionRoutes);
 if(ENV.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
-    app.get("/{*}",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-    })
+    app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
 }
 const startServer = async() => {
     try{
