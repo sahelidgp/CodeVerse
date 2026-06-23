15b3da9ca04fe9f8d2c0cd05d7cfbc5ed41af8cd
sahelidgp <sahelimahanty25@gmail.com>
2026-06-23 20:56:21 +0530

    fixing the path issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,7 +36,7 @@ app.use("/api/sessions", sessionRoutes);
 if(ENV.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
-    app.get("*", (req, res) => {
+    app.get(["/*", "/"], (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
 });
 }
